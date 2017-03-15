9748169c528eee89869d02f3307a2d0caf807ceb
大嘴 <jiangbilly@gmail.com>
2017-03-15 17:28:17 +0800

    Add site file

---
+++ BJ/reg.php
@@ -0,0 +1,70 @@
+<?php
+
+// echo ;
+
+$logFile = realpath(dirname(__FILE__)) . "/reg.txt";
+
+$time = date('Y-m-d H:i:s');
+$ip = $_SERVER['REMOTE_ADDR'];
+$logTXT = "\n== $time [$ip] ==\n\n";
+
+
+echo $logFile;
+
+@$item = $_POST['item'];
+@$name = $_POST['name'];
+
+echo $item;
+echo $name;
+
+$logTXT = "\n$item|$name|$ip|$time\n";
+
+file_put_contents($logFile, $logTXT, FILE_APPEND | LOCK_EX);
+
+// $path = 
+
+
+/*
+
+
+
+$data = Yii::app()->request->getParam('data');
+// var_dump($data);
+foreach ($data as $row)
+{
+	$name = isset($row['name']) ? $row['name'] : '';
+	$msg = isset($row['msg']) ? $row['msg'] : '';
+	$logTXT .= "[$name]\n$msg\n\n";
+}
+
+file_put_contents($logFile, $logTXT, FILE_APPEND | LOCK_EX);
+
+$timeFile = Yii::app()->params->uploadPath . "/chat/mailtime";
+
+if (file_exists($timeFile)) {
+	$diff = (time() - filemtime($timeFile))/60;
+} else {
+	$diff = Yii::app()->params->chatEmailInterval+1;
+}
+
+if ($diff>Yii::app()->params->chatEmailInterval) {
+	file_put_contents($timeFile, $time);
+
+
+
+	$name='=?UTF-8?B?921abc?=';
+	$subject='=?UTF-8?B?'.base64_encode('921abc 有新的客服聊天').'?=';
+	$headers="From: 921abc <{memory360@126.com}>\r\n".
+		"Reply-To: {memory360@126.com}\r\n".
+		"MIME-Version: 1.0\r\n".
+		"Content-type: text/plain; charset=UTF-8";
+
+	$mailContent = "有人在921abc网站上找客服机器人聊天呢，请及时登录管理界面查看。\n\n（本邮件最多 ".Yii::app()->params->chatEmailInterval." 分钟发送一次。）\n\n".$time;
+
+
+	mail(Yii::app()->params['chatEmail'],$subject,$mailContent,$headers);
+
+
+	echo "mail!";
+}
+*/
